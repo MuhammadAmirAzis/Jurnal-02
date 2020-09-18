aade2532db120eb3a368a1b515dcb91fde4f8926
MuhammadAmirAzis <muhammadamiraziz9677@gmail.com>
2020-09-18 15:50:52 +0800

    Jurnal01.java

---
+++ Praktikum 2/Jurnal01.java
@@ -0,0 +1,8 @@
+public class Jurnal01 {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Muhammad Amir Azis");
+		System.out.println("NIM  : 6706202145");
+		System.out.println("Kelas: D3IF-44-01");
+	}
+}
